214001675340f1d2b6dd1606549be7e6a2a89864
Laurence Bautista <rorens05@gmail.com>
2022-03-06 18:32:40 +0800

    Add dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,22 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate dashboard" do
-        h3 "LINGAYEN DAGUPAN TRANSPORT COOPERATIVE"
-        h3 "Brgy. Quibaol West, Highway, Lingayen Pangasinan"
-        h3 "CDA Registration No. 9520-1010000000045365"
-        h3 "TIN N0. 737-879-958-000"
-      end
-      
-      div do
-        image_tag "/images/mission.png", style: 'width: 70%; display: block; margin: 50px auto'
-      end
-      div do
-        image_tag "/images/vision.png", style: 'width: 70%; display: block; margin: 50px auto'
-      end
-      div do
-        image_tag "/images/core-values.png", style: 'width: 70%; display: block; margin: 50px auto'
-      end
+      render 'dashboard'
     end
     
     # Here is an example of a simple dashboard with columns and panels.
